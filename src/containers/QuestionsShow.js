908e843050b570c535fb63fb2b39f4bb25183347
Allen Anderson <dande313@byu.net>
2017-11-18 22:46:22 -0500

    added Response state, which changes according to input

--- src/containers/QuestionsShow.js
+++ src/containers/QuestionsShow.js
@@ -6,23 +6,25 @@ class QuestionShow extends Component {
     constructor(props) {
         super(props);
         this.testing=this.testing.bind(this);
+        this.state = {response: ""}
   }
 
   testing(e){
     let input = parseFloat(e.target.value) || "";
     console.log(input);
     if (input < this.props.question.answer){
-      console.log ("too low")
+      this.setState({response: "Too Low"})
     } else if (input > this.props.question.answer){
-      console.log ("too high")
+      this.setState({response: "Too High"})
     } else if (input = this.props.question.answer){
-      console.log ("Perfect")
+      this.setState({response: "Perfect"})
     }
 
   }
 
 
   render(){
+    console.log(this.props)
     return(
      <div className="col-md-8">
         <h4>{this.props.question.difficulty}</h4>
@@ -31,7 +33,7 @@ class QuestionShow extends Component {
         <input 
             onChange={(e) => this.testing(e)}
           />
-        <p>{this.props.response}</p>
+        <p>{this.state.response}</p>
       </div>
     )
   }
